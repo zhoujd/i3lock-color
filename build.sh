a15620084e3f0f41e9739694e99ab4d526dbb024
Simon Puente <simonppg@gmail.com>
2020-08-01 20:38:30 -0700

    Update build.sh
    forget this

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ autoreconf -fiv
 
 BUILD_DIR=build/
 rm -rf $BUILD_DIR
-mkdir -vp $BUILD_DIR && cd $BUILD_DIR || { echo "cd $BUILD_DIR"; exit 127; }
+mkdir -vp $BUILD_DIR
+cd $BUILD_DIR || { echo "cd $BUILD_DIR"; exit 127; }
 
 ../configure --prefix=/usr --sysconfdir=/etc
 
